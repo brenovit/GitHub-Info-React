941ac998bd289d9b7adc62d0934110a928a6659b
Breno Nunes <breno.arantes97@gmail.com>
2018-03-15 08:57:34 -0300

    Stateless Componente
    Created indepent component;
    Using function instead class to define component;

--- src/Components/UserInfo.js
+++ src/Components/UserInfo.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-class UserInfo extends React.Component{
-    render(){
-        var userInfo = this.props.user;
-        if(userInfo){
-            return (
-                <div className="row">
-                    <div className="col-lg-4">
-                        <img className="img-circle" src={userInfo.avatar_url} alt="avatar" width="140" heigh="140"/>
-                        <h2>{userInfo.login}</h2>
-                        <p>{userInfo.name}</p>
-                        <p>Followers: {userInfo.followers} / Following: {userInfo.following}</p>
-                        <p><a className="btn btn-default" href={userInfo.html_url} role="button">View details</a></p>
-                    </div>
-                </div>
-            );
-        }
-        return (null);
-    }
+//criação de função para tratar o componente
+function UserInfo(props){
+    var userInfo = props.user; //Atribuo o props a uma variavel
+    //se o props estiver preenchido, monto componente HTML, se não salvo null
+    var userInfoComponent = userInfo ? 
+    (
+        <div className="row">
+            <div className="col-lg-4">
+                <img className="img-circle" src={userInfo.avatar_url} alt="avatar" width="140" heigh="140"/>
+                <h2>{userInfo.login}</h2>
+                <p>{userInfo.name}</p>
+                <p>Followers: {userInfo.followers} / Following: {userInfo.following}</p>
+                <p><a className="btn btn-default" href={userInfo.html_url} role="button">View details</a></p>
+            </div>
+        </div>
+    ) : null;
+
+    return userInfoComponent; // retorno o component do user
 }
 
 export default UserInfo;
